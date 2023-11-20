3e4012928e9f9b5bb8688dcbd2f0ef5a440ecb72
Gilmar Rodrigues <gilmarbemp@gmail.com>
2023-11-20 19:13:00 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+function App() {
+ return (
+    <div className="container">
+      <p className="text">OI EU SOU O JOAO</p>
+      <p className="text">REACT in DEVELOPER</p>
+      <p className="text">TENHO 30 ANOS</p>
+      <p className="text">SOU DESENVOLVEDOR FRONTEND COM 1 ANO DE EXPERIENCIA TRABALHANDO COMO FREELANCER .</p>
+      <p className="text">EU GOSTO DE RESOLVER PROBLEMAS USANDO DESIGN E TECNOLOGIA .</p>
+      <p className="text">COMO CONSEGUIR VAGA DE PROGRAMADOR EM 25 SEGUNDOS</p>
+    </div>
+ );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
